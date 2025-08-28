02d97244ec674d2feab061e7a80714467a849437
17mwita <francye1720@gmail.com>
2025-08-28 22:26:03 +0300

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
                 <FaMoon />
             </Button>
             <Link to='/sign-in'>
-            <Button className='bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 rounded-lg text-white'>
+            <Button color='pink' outline>
                 Sign In
             </Button>
             </Link>
